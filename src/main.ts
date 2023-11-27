bab86c09a88877d3a0daf571100011d5ebb86812
Tony Eneh <30419946+tony-eneh@users.noreply.github.com>
2023-11-27 07:31:02 +0100

    feat: add stars and image cube

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,43 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const canvas = document.querySelector('canvas.bg') as HTMLCanvasElement;
 
-// camera
+/* OBJECTS */
+// torus
+const geometry = new THREE.TorusGeometry(10, 3, 20, 100);
+const material = new THREE.MeshStandardMaterial({
+  color: 0xfaed12,
+});
+const torus = new THREE.Mesh(geometry, material);
+
+// stars
+const starGeometry = new THREE.SphereGeometry(3, 20, 20);
+const starMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+const starMesh = new THREE.Mesh(starGeometry, starMaterial);
+const stars = Array(200)
+  .fill(null)
+  .map((_) => {
+    const [x, y, z] = Array(3)
+      .fill(null)
+      .map((_) =>
+        THREE.MathUtils.randFloatSpread(
+          Math.min(window.innerWidth, window.innerHeight) * 2
+        )
+      );
+    const star = starMesh.clone();
+
+    star.position.set(x, y, z);
+
+    return star;
+  });
+
+// cube
+const pocoWatersTexture = new THREE.TextureLoader().load('poco-waters.jpg');
+const pocoCube = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshStandardMaterial({ map: pocoWatersTexture })
+);
+
+/* CAMERA */
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -15,28 +51,28 @@ camera.position.setZ(30);
 
 const orbitControls = new OrbitControls(camera, canvas);
 
-// mesh
-const geometry = new THREE.TorusGeometry(10, 3, 20, 100);
-const material = new THREE.MeshStandardMaterial({
-  color: 0xfaed12,
-});
-const torus = new THREE.Mesh(geometry, material);
-
-// lights
-const pointLight = new THREE.PointLight(0xffffff);
+/* LIGHTS */
+const pointLight = new THREE.PointLight(0xffffff, 100000);
+pointLight.position.set(50, 50, 50);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
 
-// scene
+/* SCENE */
 const scene = new THREE.Scene();
-scene.add(torus, pointLight, ambientLight);
+scene.add(torus, ...stars, pocoCube);
+scene.add(pointLight);
+scene.add(ambientLight);
 scene.add(lightHelper);
 
 const gridHelper = new THREE.GridHelper(window.innerWidth, 200);
 scene.add(gridHelper);
 
-// renderer
+// dark clouds
+// const sceneBackground = new THREE.TextureLoader().load('dark-clouds.jpeg');
+// scene.background = sceneBackground;
+
+/* RENDERER */
 const renderer = new THREE.WebGLRenderer({
   canvas,
 });
@@ -50,6 +86,8 @@ function animate() {
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+
+  pocoCube.rotation.y += 0.01;
 
   orbitControls.update();
   renderer.render(scene, camera);
